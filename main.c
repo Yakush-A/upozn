1ee0aaeec1248013d27681bdd90138fb5c73b4cd
Yakush-A <gentechalegam@gmail.com>
2025-03-29 23:09:49 +0300

    started

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "functions.c"
+
+int main(void)
+{
+    char option = 'h';
+
+    while(1)
+    {
+        option = getchar();
+        switch (option)
+        {
+        case 'h':
+            printf("hi");
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
